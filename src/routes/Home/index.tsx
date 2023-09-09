e739192d23e64b8c312fc0480a310c8075edb431
Pedro D <45217189+PedroDCN@users.noreply.github.com>
2023-09-08 23:36:16 -0300

    Add styles for first iteration of page

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,33 +1,45 @@
 import { usePokemon } from "../../contexts/PokemonContext";
 
+import "./style.css";
+
 export default function Home() {
   const { pokemons } = usePokemon();
 
   return (
-    <>
-      <ul>
-        {pokemons && Object.values(pokemons).length ? (
-          Object.values(pokemons).map((pokemon: any) => (
-            <li key={pokemon?.id}>
-              <div>
-                <img src={pokemon?.sprites?.front_default} alt='' />
-              </div>
-              <h3>
-                {pokemon?.id}. {pokemon?.name}
-              </h3>
-              <div>
-                <p>
-                  {pokemon?.types
-                    ?.map((item: any) => item.type.name)
-                    .join(" | ")}
-                </p>
-              </div>
-            </li>
-          ))
-        ) : (
-          <li>No pokemons.</li>
-        )}
-      </ul>
-    </>
+    <main>
+      <div className='container'>
+        <h1>Pokedéx</h1>
+        <ul className='pokemons-list'>
+          {pokemons && Object.values(pokemons).length ? (
+            Object.values(pokemons).map((pokemon: any) => (
+              <li
+                key={pokemon?.id}
+                className={`pokemon-card ${pokemon.types[0].type.name}`}
+              >
+                <div className='card-image-container'>
+                  <img
+                    className='card-image'
+                    src={pokemon?.sprites?.front_default}
+                    alt={pokemon.name}
+                  />
+                </div>
+                <h3>
+                  {pokemon?.id}. {pokemon?.name}
+                </h3>
+                <div>
+                  <p className='card-type'>
+                    {pokemon?.types
+                      ?.map((item: any) => item.type.name)
+                      .join(" | ")}
+                  </p>
+                </div>
+              </li>
+            ))
+          ) : (
+            <li>No pokemons.</li>
+          )}
+        </ul>
+      </div>
+    </main>
   );
 }
